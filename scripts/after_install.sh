b675d1bb44044ec0dc870e5ef835b3e64e461785
vikrant1992 <vikrant1013@gmail.com>
2019-09-19 15:35:49 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,6 @@
 # starting nginx service
 
 sudo sh /home/ec2-user/permission.sh
-cp -rf /app/magento/emimall-prod/app/code /app/magento/app/code
-cp -rf /app/magento/emimall-prod/app/design /app/magento/app/design
+cp -rf /app/magento/emimall-uat/app/code /app/magento/app/code
+cp -rf /app/magento/emimall-uat/app/design /app/magento/app/design
 
